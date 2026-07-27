11e9992a86bbd3b0e04bfc8f656cd02aa51fa797
Donovan <donovan.erik@gmail.com>
2026-07-27 12:08:12 +0200

    chore: refresh cache for bottom navigation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'la-word-v15';
+const CACHE = 'la-word-v16';
 const APP_SHELL = [
   './',
   './index.html',
--- tests/service-worker.test.js
+++ tests/service-worker.test.js
@@ -83,7 +83,7 @@ test('service worker refreshes cached assets for the flat slider transition', ()
     'utf8'
   );
 
-  assert.match(source, /const CACHE = 'la-word-v15';/);
+  assert.match(source, /const CACHE = 'la-word-v16';/);
 });
 
 test('SKIP_WAITING message activates the waiting worker', () => {
